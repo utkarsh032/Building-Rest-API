61dfb9e804245c3910c0b56a7ffebbe4f77b416e
utkarsh032 <utkarshhraj525@gmail.com>
2023-07-02 16:54:03 +0530

    Backend - Adding Routes

---
+++ routes/places-routes.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+const router = express.Router()
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrappers in the world!',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: '20 W 34TH St, New York, NY 10001',
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrappers in the world!',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: '20 W 34TH St, New York, NY 10001',
+    creator: 'u1'
+  }
+]
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId
+  })
+  res.json({ place })
+})
+
+module.exports = router
