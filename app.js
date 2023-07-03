42ef3810917966a2babb6fa12bf72c589ddbc797
utkarsh032 <utkarshhraj525@gmail.com>
2023-07-03 07:43:52 +0530

    Handling Error

--- app.js
+++ app.js
@@ -5,5 +5,14 @@ const placesRoutes = require('./routes/places-routes')
 
 const app = express()
 
-app.use('/api/places', placesRoutes)
+app.use('/api/places', placesRoutes) // => /api/places...
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error)
+  }
+  res.status(error.code || 500)
+  res.json({ message: error.message || 'An unknown error occurred!' })
+})
+
 app.listen(5000)
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -6,33 +6,30 @@ const DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Empire State Building',
-    description: 'One of the most famous sky scrappers in the world!',
+    description: 'One of the most famous sky scrapers in the world!',
     location: {
       lat: 40.7484474,
       lng: -73.9871516
     },
-    address: '20 W 34TH St, New York, NY 10001',
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrappers in the world!',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516
-    },
-    address: '20 W 34TH St, New York, NY 10001',
+    address: '20 W 34th St, New York, NY 10001',
     creator: 'u1'
   }
 ]
 
 router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid
+  const placeId = req.params.pid // { pid: 'p1' }
+
   const place = DUMMY_PLACES.find(p => {
     return p.id === placeId
   })
-  res.json({ place })
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided id.')
+    error.code = 404
+    throw error
+  }
+
+  res.json({ place }) // => { place } => { place: place }
 })
 
 router.get('/user/:uid', (req, res, next) => {
@@ -41,6 +38,13 @@ router.get('/user/:uid', (req, res, next) => {
   const place = DUMMY_PLACES.find(p => {
     return p.creator === userId
   })
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided user id.')
+    error.code = 404
+    return next(error)
+  }
+
   res.json({ place })
 })
 
