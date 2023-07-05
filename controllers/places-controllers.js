b572aadd7ea4074b7dd386f85015f97fee799544
utkarsh032 <utkarshhraj525@gmail.com>
2023-07-05 20:33:11 +0530

    Post Request : POSTMAN

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,17 +1,11 @@
+// const uuid = require('uuid')
+// const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid');
+
+
 const HttpError = require('../models/http-error')
 
 const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  },
   {
     id: 'p1',
     title: 'Empire State Building',
@@ -58,5 +52,23 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place })
 }
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body
+
+  const createdPlace = {
+    id: uuidv4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  }
+
+  DUMMY_PLACES.push(createdPlace)
+
+  res.status(201).json({ place: createdPlace })
+}
+
 exports.getPlaceById = getPlaceById
 exports.getPlaceByUserId = getPlaceByUserId
+exports.createPlace = createPlace
