93ac663406a900031c0d89f09f907aafc1dec320
utkarsh032 <utkarshhraj525@gmail.com>
2023-07-05 23:19:03 +0530

    Path routes to Update Places

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,7 +1,6 @@
 // const uuid = require('uuid')
 // const { v4: uuidv4 } = require('uuid')
-const { v4: uuidv4 } = require('uuid');
-
+const { v4: uuidv4 } = require('uuid')
 
 const HttpError = require('../models/http-error')
 
@@ -69,6 +68,24 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace })
 }
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body
+  const placeId = req.params.pid
+
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) }
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId)
+  updatedPlace.title = title
+  updatedPlace.description = description
+
+  DUMMY_PLACES[placeIndex] = updatedPlace
+
+  res.status(200).json({ place: updatedPlace })
+}
+
+const deletePlace = (req, res, next) => {}
+
 exports.getPlaceById = getPlaceById
 exports.getPlaceByUserId = getPlaceByUserId
 exports.createPlace = createPlace
+exports.updatePlace = updatePlace
+exports.deletePlace = deletePlace
