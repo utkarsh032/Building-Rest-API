f3dbca9b9d2730f16696b873cc22fcf1e2561d92
utkarsh032 <utkarshhraj525@gmail.com>
2023-07-07 08:23:08 +0530

    validating API Input

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,5 +1,7 @@
-const { v4: uuidv4 } = require('uuid');
-const HttpError = require('../models/http-error');
+const { v4: uuidv4 } = require('uuid')
+const { validationResult } = require('express-validator')
+
+const HttpError = require('../models/http-error')
 
 let DUMMY_PLACES = [
   {
@@ -13,43 +15,49 @@ let DUMMY_PLACES = [
     address: '20 W 34th St, New York, NY 10001',
     creator: 'u1'
   }
-];
+]
 
 const getPlaceById = (req, res, next) => {
-  const placeId = req.params.pid; // { pid: 'p1' }
+  const placeId = req.params.pid // { pid: 'p1' }
 
   const place = DUMMY_PLACES.find(p => {
-    return p.id === placeId;
-  });
+    return p.id === placeId
+  })
 
   if (!place) {
-    throw new HttpError('Could not find a place for the provided id.', 404);
+    throw new HttpError('Could not find a place for the provided id.', 404)
   }
 
-  res.json({ place }); // => { place } => { place: place }
-};
+  res.json({ place }) // => { place } => { place: place }
+}
 
 // function getPlaceById() { ... }
 // const getPlaceById = function() { ... }
 
 const getPlacesByUserId = (req, res, next) => {
-  const userId = req.params.uid;
+  const userId = req.params.uid
 
   const places = DUMMY_PLACES.filter(p => {
-    return p.creator === userId;
-  });
+    return p.creator === userId
+  })
 
   if (!places || places.length === 0) {
     return next(
       new HttpError('Could not find places for the provided user id.', 404)
-    );
+    )
   }
 
-  res.json({ places });
-};
+  res.json({ places })
+}
 
 const createPlace = (req, res, next) => {
-  const { title, description, coordinates, address, creator } = req.body;
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    console.log(errors)
+    throw new HttpError('Invalid inputs passed, please check your data', 422)
+  }
+
+  const { title, description, coordinates, address, creator } = req.body
   // const title = req.body.title;
   const createdPlace = {
     id: uuid(),
@@ -58,35 +66,35 @@ const createPlace = (req, res, next) => {
     location: coordinates,
     address,
     creator
-  };
+  }
 
-  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
+  DUMMY_PLACES.push(createdPlace) //unshift(createdPlace)
 
-  res.status(201).json({ place: createdPlace });
-};
+  res.status(201).json({ place: createdPlace })
+}
 
 const updatePlace = (req, res, next) => {
-  const { title, description } = req.body;
-  const placeId = req.params.pid;
+  const { title, description } = req.body
+  const placeId = req.params.pid
 
-  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
-  updatedPlace.title = title;
-  updatedPlace.description = description;
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) }
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId)
+  updatedPlace.title = title
+  updatedPlace.description = description
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;
+  DUMMY_PLACES[placeIndex] = updatedPlace
 
-  res.status(200).json({ place: updatedPlace });
-};
+  res.status(200).json({ place: updatedPlace })
+}
 
 const deletePlace = (req, res, next) => {
-  const placeId = req.params.pid;
-  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
-  res.status(200).json({ message: 'Deleted place.' });
-};
+  const placeId = req.params.pid
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId)
+  res.status(200).json({ message: 'Deleted place.' })
+}
 
-exports.getPlaceById = getPlaceById;
-exports.getPlacesByUserId = getPlacesByUserId;
-exports.createPlace = createPlace;
-exports.updatePlace = updatePlace;
-exports.deletePlace = deletePlace;
+exports.getPlaceById = getPlaceById
+exports.getPlacesByUserId = getPlacesByUserId
+exports.createPlace = createPlace
+exports.updatePlace = updatePlace
+exports.deletePlace = deletePlace
